02918e73cb65f377e423c17bd1ec610a208790af
upfy@qq.com <upfy@qq.com>
2015-02-15 22:11:38 +0800

    ok

--- workspace/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p /code/local
-cd /code/local
-ssh-keygen -N "" -t dsa -b 1024 -f rsync-key
-
-
--- workspace/username.www/codersyn.sh
+++ workspace/username.www/codersyn.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
-USER=luofei
+USER=username
+HOST=username.www.test.com
 PWD=$( pwd | sed "s/\//#/g")
+ssh-add ./rsync-key
+chmod 600 ./rsync-key
 fswatch -0 . |  while read -d "" file ; do \
     FILE_PATH=$(echo $file | sed "s/\//#/g")
     SHORT_FILE_PATH=$(echo $FILE_PATH | sed "s/$PWD//g")
     DIR=`echo $SHORT_FILE_PATH|awk -F '#' '{print $2}'`
     #如果是目录
     if [  -d "$DIR" ]; then  
-        date +%H:%M:%S && /usr/bin/rsync -iru --exclude .git  --delete -e 'ssh -i ./rsync-key -p 22' $DIR/ www-data@103.227.76.203:/code/$USER.$DIR
+        date +%H:%M:%S && /usr/bin/rsync -iru --exclude .git  --delete -e 'ssh -i ./rsync-key -p 222' $DIR/ www-data@$HOST:/code/$USER.$DIR
     fi  
 done
